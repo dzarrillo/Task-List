a67c7b1f399bb67c6d45ae2500830d0bdadef710
Don Zarrillo Jr <donzarrillo@gmail.com>
2024-08-18 17:53:55 -0400

    first commit

---
+++ ui/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { AddTaskForm } from "./components/AddTaskForm";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Task } from "./components/Task";
+import axios from "axios";
+import { API_URL } from "./utils";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get(API_URL);
+
+      setTasks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AddTaskForm fetchTasks={fetchTasks} />
+      {tasks.map((task) => (
+        <Task task={task} key={task.id} fetchTasks={fetchTasks} />
+      ))}
+    </ThemeProvider>
+  );
+}
+
+export default App;
