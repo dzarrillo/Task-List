16d2e0956e276e4ca6e2100d7b4eed418066d134
Don Zarrillo Jr <donzarrillo@gmail.com>
2024-08-19 12:54:59 -0400

    changed url to dynamodb backend

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:3001/task";
+// export const API_URL = "http://localhost:3001/task";
+export const API_URL =
+  "https://kwxjibhotnz3wjwfom7jk6cqju0apvea.lambda-url.us-east-1.on.aws/task";
